1572a0d26d4b78fce62c0ec9f94c3eb13ffe01bc
Marcin Daczkowski <merdacz@gmail.com>
2016-07-08 21:49:45 +0200

    initial commit

---
+++ Octopus.Playground/Octopus.Playground.Test/SomeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Octopus.Playground.Test
+{
+    [TestClass]
+    public class SomeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
